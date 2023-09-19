0a1aad44e2163b99b97d40eb556568b121c8b30f
dganesh22 <dganesh2022@gmail.com>
2023-09-19 18:36:39 +0530

    completd functional component

---
+++ src/component/Ex2.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Ex2 extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Git User Finder (Class Component) </h1>
+            </div>
+        )
+    }
+}
+
+export default Ex2
